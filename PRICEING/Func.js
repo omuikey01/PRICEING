8f4ce9d641c98a1f17ccb142ce6edfc4332c288e
Om Uikey <142112592+omuikey01@users.noreply.github.com>
2023-08-12 23:38:52 +0530

    Add files via upload

---
+++ PRICEING/Func.js
@@ -0,0 +1,18 @@
+document.getElementById("YY").addEventListener("click". myYY);
+function myYY(){
+	// let YYY = document.getElementById("myYY");
+	// if(YYY == "on"){
+	// 	document.getElementById('BoxesH').style.display="none";
+	// 	document.getElementById('Boxes').style.display="block";
+	// }
+	alert("Year");
+}
+document.getElementById("MM").addEventListener("click". myMM);
+function myMM(){
+	// let MMM = document.getElementById("myYY");
+	// if(MMM == "on"){
+	// 	document.getElementById('BoxesH').style.display="block";
+	// 	document.getElementById('Boxes').style.display="none";
+	// }
+	alert("Month");
+}
